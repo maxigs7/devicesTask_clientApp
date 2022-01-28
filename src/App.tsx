e802cb8fed1dfa9cb3b2ee1db65bb56c67002aff
Maximiliano Gonzalez Schweizer <maxi.g14@hotmail.com>
2022-01-28 15:48:37 -0300

    Base structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './styles/App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <h1 className={styles.title}>NinjaRMM - Device Mananger</h1>
+    </div>
+  );
+}
+
+export default App;
